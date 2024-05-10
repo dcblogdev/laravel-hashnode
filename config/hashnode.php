2e9b924a975a052cc551e9a0d46194cc9a8812f8
David Carr <dave@dcblog.dev>
2024-05-11 00:58:41 +0100

    Update default posts per page in Hashnode config
    The default value for posts per page in the Hashnode configuration file has been increased from 9 to 18. This change will allow more posts to be displayed at a time, providing a better user experience.

--- config/hashnode.php
+++ config/hashnode.php
@@ -3,5 +3,5 @@
 return [
     'url' => env('HASHNODE_URL', 'https://gql.hashnode.com/'),
     'host' => env('HASHNODE_HOST'),
-    'perPage' => env('HASHNODE_PER_PAGE', 9),
+    'perPage' => env('HASHNODE_PER_PAGE', 18),
 ];
